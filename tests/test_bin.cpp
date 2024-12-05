8421e0d94fce0fcb6e38b7f2c33d355ab2057165
Stephen Yang <stephy.yang@mail.utoronto.ca>
2024-12-05 11:10:10 -0500

    [cmake] use include_directory instead of relative includes

--- tests/test_bin.cpp
+++ tests/test_bin.cpp
@@ -1,35 +1,51 @@
 //
 // Created by viciopoli on 20/11/24.
 //
-
 #include <gtest/gtest.h>
 #include <iostream>
+#include <vector>
 
-#include "../include/sim/sinusoid_sim.hpp"
-#include "../include/bin.hpp"
+#include "sim/sinusoid_sim.hpp"
+#include "filter/ekf.hpp"
 
+std::tuple<double, double, double, double, double> testEKF(double amp, double freq, double phase)
+{
+  EKF ekf(1, 0.1, 0.1);
+  ekf.initialize(80., 1., 1., 1., 1.);
 
-bool testBin(double freq_target, double freq) {
-    Bin bin(10, 0.1, 0.1, freq_target, 1., 1.);
+  SinusoidSim<double> sim_x(amp, freq, phase);
+  SinusoidSim<double> sim_y(amp + 3, freq, phase);
 
-    SinusoidSim<double> sim_x(10, freq, 1., 10);
-    SinusoidSim<double> sim_y(10, freq, 1., 30);
+  double delta = 0.01;
 
-    double delta = 1. / (2. * Hz2rad(freq) + 1.);
+  for (int i = 0; i < 10'000; ++i) {
+    double t = i * delta;
+    double x = sim_x(t);
+    double y = sim_y(t);
+    ekf.update(x, y, t);
+  }
 
-    for (int i = 0; i < 10'000; ++i) {
-        double t = i * delta;
-        double x = sim_x(t);
-        double y = sim_y(t);
-        bin.update(x, y, t);
-    }
-
-    std::cout << bin << std::endl;
-    // return the estimated parameters
-    return bin.is_stable();
+  std::cout << ekf << std::endl;
+  // return the estimated parameters
+  return std::make_tuple(
+    ekf.getRadS(),
+    ekf.getAmplX(),
+    ekf.getAmplY(),
+    ekf.getPhaseX(),
+    ekf.getPhaseY());
 }
 
-TEST(Bin, Estimation) {
-    EXPECT_TRUE(testBin(100., 100.));
-    EXPECT_FALSE(testBin(10., 15.));
+TEST(EKF, Estimation) {
+  double amp = 4.0;
+  double freq = 100.0;
+  double phase = 1.0;
+
+  auto [omega, A_x, A_y, phi_x, phi_y] = testEKF(amp, freq, phase);
+
+  EXPECT_NEAR(omega, freq, 1.);
+  EXPECT_NEAR(A_x, amp, 1.);
+  EXPECT_NEAR(A_y, amp + 3, 1.);
+
+  EXPECT_NEAR(phi_x, phase, 1.);
+  EXPECT_NEAR(phi_y, phase, 1.);
 }
--- tests/test_ekf.cpp
+++ tests/test_ekf.cpp
@@ -5,42 +5,47 @@
 #include <iostream>
 #include <vector>
 
-#include "../include/sim/sinusoid_sim.hpp"
-#include "../include/filter/ekf.hpp"
+#include "sim/sinusoid_sim.hpp"
+#include "filter/ekf.hpp"
 
-std::tuple<double, double, double, double, double> testEKF(double amp, double freq, double phase) {
-    EKF ekf(1, 0.1, 0.1);
-    ekf.initialize(80., 1., 1., 1., 1.);
+std::tuple<double, double, double, double, double> testEKF(double amp, double freq, double phase)
+{
+  EKF ekf(1, 0.1, 0.1);
+  ekf.initialize(80., 1., 1., 1., 1.);
 
-    SinusoidSim<double> sim_x(amp, freq, phase);
-    SinusoidSim<double> sim_y(amp + 3, freq, phase);
+  SinusoidSim<double> sim_x(amp, freq, phase);
+  SinusoidSim<double> sim_y(amp + 3, freq, phase);
 
-    double delta = 0.01;
+  double delta = 0.01;
 
-    for (int i = 0; i < 10'000; ++i) {
-        double t = i * delta;
-        double x = sim_x(t);
-        double y = sim_y(t);
-        ekf.update(x, y, t);
-    }
+  for (int i = 0; i < 10'000; ++i) {
+    double t = i * delta;
+    double x = sim_x(t);
+    double y = sim_y(t);
+    ekf.update(x, y, t);
+  }
 
-    std::cout << ekf << std::endl;
-    // return the estimated parameters
-    return std::make_tuple(ekf.getRadS(), ekf.getAmplX(), ekf.getAmplY(), ekf.getPhaseX(), ekf.getPhaseY());
+  std::cout << ekf << std::endl;
+  // return the estimated parameters
+  return std::make_tuple(
+    ekf.getRadS(),
+    ekf.getAmplX(),
+    ekf.getAmplY(),
+    ekf.getPhaseX(),
+    ekf.getPhaseY());
 }
 
 TEST(EKF, Estimation) {
-    double amp = 4.0;
-    double freq = 100.0;
-    double phase = 1.0;
+  double amp = 4.0;
+  double freq = 100.0;
+  double phase = 1.0;
 
-    auto [omega, A_x, A_y, phi_x, phi_y] = testEKF(amp, freq, phase);
+  auto [omega, A_x, A_y, phi_x, phi_y] = testEKF(amp, freq, phase);
 
-    EXPECT_NEAR(omega, freq, 1.);
-    EXPECT_NEAR(A_x, amp, 1.);
-    EXPECT_NEAR(A_y, amp + 3, 1.);
+  EXPECT_NEAR(omega, freq, 1.);
+  EXPECT_NEAR(A_x, amp, 1.);
+  EXPECT_NEAR(A_y, amp + 3, 1.);
 
-    EXPECT_NEAR(phi_x, phase, 1.);
-    EXPECT_NEAR(phi_y, phase, 1.);
+  EXPECT_NEAR(phi_x, phase, 1.);
+  EXPECT_NEAR(phi_y, phase, 1.);
 }
-
