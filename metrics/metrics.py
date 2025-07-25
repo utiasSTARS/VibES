0fe57110a44ebd55c2b5e79d1c9bd08ac35baabe
Stephen Yang <stephy.yang@mail.utoronto.ca>
2025-07-25 16:12:47 -0400

    [metrics] update

--- metrics/metrics.py
+++ metrics/metrics.py
@@ -1,10 +1,31 @@
 import numpy as np
 import h5py
 
+from metavision_core.event_io import EventsIterator
+
 import argparse
 
 
-def read_events_from_hdf5(file_path, time_window_us=-1):
+def h5_tree(val, pre="", out=""):
+    length = len(val)
+    for key, val in val.items():
+        length -= 1
+        if length == 0:  # the last item
+            if type(val) == h5py._hl.group.Group:
+                out += pre + "└── " + key + "\n"
+                out = h5_tree(val, pre + "    ", out)
+            else:
+                out += pre + "└── " + key + f" {val.shape}\n"
+        else:
+            if type(val) == h5py._hl.group.Group:
+                out += pre + "├── " + key + "\n"
+                out = h5_tree(val, pre + "│   ", out)
+            else:
+                out += pre + "├── " + key + f" {val.shape}\n"
+    return out
+
+
+def read_events_from_hdf5(file_path, time_window_us):
     import h5py
 
     initial_time = -1
@@ -19,7 +40,7 @@ def read_events_from_hdf5(file_path, time_window_us=-1):
             delta = ts - initial_time
             events.append((ts, x, y, p))
 
-            if time_window_us != -1 and delta > time_window_us:
+            if time_window_us != -1.0 and delta > time_window_us:
                 break  # Stop when reaching the time window or max events
 
     # print info
@@ -27,16 +48,44 @@ def read_events_from_hdf5(file_path, time_window_us=-1):
     return events
 
 
+class Metrics:
+    def __init__(self, cam_w, cam_h, events):
+        self.cam_w = cam_w
+        self.cam_h = cam_h
+        self.events = events
+
+    def point_distribution(self):
+        # Compute point distribution metric
+
+        pass
+
+
 # Example usage:
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process HDF5 event data.")
     parser.add_argument("file_path", type=str, help="Path to the HDF5 file")
     parser.add_argument(
-        "--time_window_us", type=int, default=-1, help="Time window in microseconds"
+        "--time_window_us",
+        "-t",
+        type=float,
+        default=-1.0,
+        help="Time window in microseconds",
+    )
+    parser.add_argument(
+        "--delta_t",
+        type=int,
+        default=1000,
+        help="Delta time in microseconds for event iterator",
     )
     parser.add_argument("--pd", action="store_true", help="Metric: Point Distribution")
     args = parser.parse_args()
 
-    # Example file path (adjust as needed)
-    file_path = args.file_path
-    time_window_us = args.time_window_us
+    # Initialize RawReader
+    mv_iterator = EventsIterator(input_path=args.file_path, delta_t=args.delta_t)
+    height, width = mv_iterator.get_size()  # Camera Geometry
+
+    print(f"Processing file: {args.file_path}")
+    print(f"Camera Geometry: {height}x{width}")
+
+    # Read events from the HDF5 file
+    # events = read_events_from_hdf5(file_path, time_window_us)
